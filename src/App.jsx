b9b64da31210cb8cbe6dc85844b192b421112f71
Sara Gibbons <saragibby@gmail.com>
2022-09-26 16:43:57 +0000

    revert name change from testing

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const SITE_PROPS = {
-  name: "TEST Grenier",
+  name: "Alexandrie Grenier",
   title: "Web Designer & Content Creator",
   email: "alex@example.com",
   gitHub: "microsoft",
